e0526dc6765093374ec7c32e00e70681b25e85fc
Evgeniy_Kudryashov <gagarinru84@yandex.ru>
2024-12-14 14:21:12 +0300

    first version for docker_copose

--- docker_compose/simple_project/app/entrypoint.sh
+++ docker_compose/simple_project/app/entrypoint.sh
@@ -4,9 +4,7 @@ python manage.py collectstatic --noinput
 
 python manage.py makemigrations
 python manage.py migrate
-
 python manage.py createcachetable
-
 python manage.py createsuperuser\
    --noinput\
    --username $DJANGO_SUPERUSER_USERNAME\
